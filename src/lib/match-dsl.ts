ea3541d24bbc203e78e3e1121e0da958c3c545d2
lostintime <lostintime.dev@gmail.com>
2017-12-11 14:57:10 +0200

    Dependencies update

--- src/lib/match-dsl.ts
+++ src/lib/match-dsl.ts
@@ -8,19 +8,27 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-export * from "typematcher/dist/lib/matchers"
-export {
-  CaseMatch,
-  CaseMiss,
-  CaseResult,
-  MatchCase,
-  caseWhen,
-  caseAny,
-  caseDefault,
-  caseId,
-  caseThrow
-} from "typematcher/dist/lib/match-dsl"
 import { MatchCase, matchWith as matchStrictWith } from "typematcher"
 import { IO } from "funfix-effect"
 
-export * from "./match-dsl"
+/**
+ * matchWith is same as match() only with inverse arguments order,
+ * this can be used to pre-build matchers and save some CPU cycles
+ */
+export function matchWith<R>(...cases: MatchCase<R>[]): (val: any) => IO<R> {
+  // Prepare matcher
+  const matcher = matchStrictWith<R>(...cases)
+
+  return function value(val: any): IO<R> {
+    return IO.once(() => matcher(val))
+  }
+}
+
+/**
+ * Match value against all given matchers, return IO on match or failure
+ */
+export function match(val: any): <R>(...cases: MatchCase<R>[]) => IO<R> {
+  return function cases<R>(...cases: MatchCase<R>[]): IO<R> {
+    return matchWith(...cases)(val)
+  }
+}
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,62 +1,72 @@
-import {expect} from 'chai';
-import {match, matchWith} from "../lib";
+/*
+ * Copyright 2017 by TypeMatcher developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { expect } from "chai"
+import { match, matchWith } from "../lib"
 import {
   caseAny, caseDefault, caseId, caseThrow, caseWhen, isNumber, isString,
   isValue
-} from "typematcher";
-import {Failure, Some, Success} from "funfix-core";
+} from "typematcher"
+import { Failure, Some, Success } from "funfix-core"
 
-describe('Match DSL', function () {
+describe("Match DSL", function () {
   const toString = (obj: Object) => {
     try {
       return JSON.stringify(obj)
     } catch (e) {
       return obj.toString()
     }
-  };
+  }
 
-  describe('match', () => {
-    it('will return first matched case value', () => {
+  describe("match", () => {
+    it("will return first matched case value", () => {
       expect(toString(match(10)(caseId(isValue(10))).run().value()))
-        .equals(toString(Some(Success(10))), 'caseId returns input value if matched');
+        .equals(toString(Some(Success(10))), "caseId returns input value if matched")
 
       expect(toString(match(10)(caseWhen(isString)(s => `got string ${s}`), caseWhen(isNumber)(n => `got number ${n}`)).run().value()))
-        .equals(toString(Some(Success("got number 10"))), 'returned first matched');
+        .equals(toString(Some(Success("got number 10"))), "returned first matched")
 
       expect(toString(match("hello")(caseWhen(isNumber)(n => ""), caseAny(v => `got value "${v}"`)).run().value()))
-        .equals(toString(Some(Success('got value "hello"'))), 'caseAny matches any value');
+        .equals(toString(Some(Success('got value "hello"'))), "caseAny matches any value")
 
       expect(toString(match(333)(caseDefault(() => 20), caseId(isNumber)).run().value()))
-        .equals(toString(Some(Success(20))), 'caseDefault returns first');
-    });
+        .equals(toString(Some(Success(20))), "caseDefault returns first")
+    })
 
-    it('will throw if no case matched', () => {
+    it("will throw if no case matched", () => {
       expect(toString(match(10)().run().value()))
-        .equals(toString(Some(Failure(new Error('message ignored as not JSON.stringified')))), 'mismatch returns a failure');
+        .equals(toString(Some(Failure(new Error("message ignored as not JSON.stringified")))), "mismatch returns a failure")
 
       expect(toString(match(10)().run().recover(e => e.toString()).value()))
-        .equals(toString(Some(Success('Error: No match'))), 'failure contains exact error');
+        .equals(toString(Some(Success("Error: No match"))), "failure contains exact error")
 
-      expect(toString(match(10)(caseThrow(new Error('a custom error message'))).run().recover(e => e.toString()).value()))
-        .equals(toString(Some(Success('Error: a custom error message'))), 'failure message matches');
+      expect(toString(match(10)(caseThrow(new Error("a custom error message"))).run().recover(e => e.toString()).value()))
+        .equals(toString(Some(Success("Error: a custom error message"))), "failure message matches")
 
-      expect(toString(match(10)(caseThrow(new Error('a custom error message'))).run().recover(e => e.toString()).value()))
-        .not.equals(toString(Some(Success('Error: other error message'))), 'failure message matches');
-    });
-  });
+      expect(toString(match(10)(caseThrow(new Error("a custom error message"))).run().recover(e => e.toString()).value()))
+        .not.equals(toString(Some(Success("Error: other error message"))), "failure message matches")
+    })
+  })
 
-  describe('matchWith', () => {
-    it('will return first matched case value', () => {
+  describe("matchWith", () => {
+    it("will return first matched case value", () => {
       expect(toString(match(15)(caseId(isValue(15))).run().value()))
-        .equals(toString(Some(Success(15))), 'caseId returns input value if matched');
+        .equals(toString(Some(Success(15))), "caseId returns input value if matched")
 
       expect(toString(match(15)(caseId(isValue(15))).run().value()))
-        .not.equals(toString(Some(Success(51))), 'caseId returns input value if matched');
-    });
+        .not.equals(toString(Some(Success(51))), "caseId returns input value if matched")
+    })
 
-    it('will throw if no case matched', () => {
+    it("will throw if no case matched", () => {
       expect(toString(matchWith()(10).run().recover(e => e.toString()).value())) // recovering to get message
-        .equals(toString(Some(Success('Error: No match'))), 'mismatch returns a failure, with "No match" message');
-    });
-  });
-});
+        .equals(toString(Some(Success("Error: No match"))), 'mismatch returns a failure, with "No match" message')
+    })
+  })
+})
